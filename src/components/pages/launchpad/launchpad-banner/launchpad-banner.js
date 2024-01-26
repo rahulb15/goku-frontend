1ec5dbcc57fa0bc2b4901f720e96eb1aa07abbe2
rahul <gamefever91@live.com>
2024-01-26 14:22:25 +0530

    fix bugs and build

--- src/components/pages/launchpad/launchpad-banner/launchpad-banner.js
+++ src/components/pages/launchpad/launchpad-banner/launchpad-banner.js
@@ -1,11 +1,27 @@
-//import React, { Component } from 'react'
+import React, { useEffect, useState } from "react";
 import { Button } from "reactstrap";
 //import LaunchpadCountDown from './launchpad-countdown'
 //import {fetchPriorityPass} from "../../launchpad-detail/launchpad-banner/getPriorityPass.action"
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { upcomingProjects } from "../../launchpad/upcoming-projects/upcomingProjectList";
+import { useParams } from "react-router-dom";
 
 const LaunchpadBanner = () => {
+  const { passDetails } = useSelector((state) => state.passDetails);
+  console.log("passDetails", passDetails);
+  const params = useParams();
+  const [project, setProject] = useState("db-cooper");
   const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   const project = upcomingProjects.filter((data) => {
+  //     return data.id == params.id;
+  //   });
+  //   setProject(project[0]);
+  // }, []);
+  // console.log("project", project);
+
   return (
     <div>
       <div className="launch_bannerOuter" id="slide1">
@@ -29,13 +45,23 @@ const LaunchpadBanner = () => {
                 <ul>
                   <li>
                     <span>Mint Date</span>
-                    <strong>January 14th, 2023</strong>
+                    <strong>January 14th, 2024</strong>
                   </li>
                 </ul>
                 <ul>
                   <li>
                     <span>Total NFTS</span>
-                    <strong>2,777</strong>
+                    <strong>
+                    {project.projectName == "Priority Pass" ? (
+          <div className="fixvalue medium">
+            {passDetails ? passDetails.priorityPassNumber : null}
+          </div>
+        ) : (
+          <div className="fixvalue medium">
+            {passDetails ? passDetails.dbCooperTotalNumber : null}
+          </div>
+        )}
+                    </strong>
                   </li>
                   <li>
                     <span>Max Mint Allocation</span>
@@ -49,7 +75,18 @@ const LaunchpadBanner = () => {
                   </li>
                   <li>
                     <span>Total Minted</span>
-                    <strong>0</strong>
+                    {/* <strong></strong> */}
+                    <strong>
+                    {project.projectName == "Priority Pass" ? (
+          <div className="value medium">
+            {passDetails ? passDetails.priorityPassMintedNumber : null}
+          </div>
+        ) : (
+          <div className="value medium">
+            {passDetails ? passDetails.dbCooperMintedNumber : null}
+          </div>
+        )}
+                    </strong>
                   </li>
                 </ul>
               </div>
